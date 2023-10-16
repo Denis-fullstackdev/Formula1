3892add45bb1b8fd03b1e2d94b2a038c0ba12e7f
Denis-fullstackdev <d.mozenic@gmail.com>
2023-10-16 20:51:14 +0200

    init project

---
+++ src/main/java/com/corsojava/springboot/Formula1SpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.corsojava.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formula1SpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formula1SpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corsojava/springboot/Formula1SpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corsojava.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1SpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
